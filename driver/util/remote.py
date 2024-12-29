359cd969b827f975b90c291a6696abbd2f431b2d
Kaifei Peng <2534164734@qq.com>
2024-12-29 12:45:30 +0000

    fix error logging of remote cmd

--- driver/util/remote.py
+++ driver/util/remote.py
@@ -62,7 +62,8 @@ class RemoteMachine:
             errors = stderr.read().decode()
 
             if errors and not errors.startswith("[sudo]"):
-                print(f"Errors:\n{errors}")
+                print(f"Error occured when executing {full_command}")
+                print(f"Errors:\n{errors}\n")
             else:
                 if output_file:
                     print(f"Command output has been redirected to {output_file}")
