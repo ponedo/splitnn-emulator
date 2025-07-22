ade39397101cb5811d8fc29c3e88773a87abff0c
Kaifei Peng <2534164734@qq.com>
2025-07-22 06:01:42 +0000

    add max_user_process ulimit setting

--- agent/code/network/envManage.go
+++ agent/code/network/envManage.go
@@ -455,9 +455,12 @@ func setRlimits() {
 
 	setRlimit(unix.RLIMIT_MEMLOCK, "RLIMIT_MEMLOCK") // ulimit -l
 	setRlimit(unix.RLIMIT_DATA, "RLIMIT_DATA")       // ulimit -m
+	setRlimit(unix.RLIMIT_NPROC, "RLIMIT_NPROC")     // Set RLIMIT_NPROC (max user processes)
 
 	_ = unix.Getrlimit(unix.RLIMIT_MEMLOCK, &rlim)
 	fmt.Printf("After rlimit setting: RLIMIT_MEMLOCK = Cur=%d Max=%d\n", rlim.Cur, rlim.Max)
+	_ = unix.Getrlimit(unix.RLIMIT_NPROC, &rlim)
+	fmt.Printf("After rlimit setting: RLIMIT_NPROC = Cur=%d Max=%d\n", rlim.Cur, rlim.Max)
 }
 
 func prepareRootfs(dockerImageName string) {
